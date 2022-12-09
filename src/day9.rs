367e8a3cb00fbc6fe88c230b5c0ce9468a242881
Andreas Tollkötter <andreas.tollkoetter@flipapp.de>
2022-12-09 08:47:45 +0100

    day 9 star 2

--- src/day9.rs
+++ src/day9.rs
@@ -1,7 +1,5 @@
-use std::cmp::{max, min};
 use std::collections::HashSet;
-use std::fs;
-use std::iter::repeat;
+use std::{fs, iter};
 use itertools::Itertools;
 
 
@@ -26,84 +24,86 @@ impl Direction {
     }
 }
 
-#[derive(Debug, Clone)]
-struct Move {
-    direction: Direction,
-    distance: usize,
-}
-
-fn read_input() -> Vec<Move> {
+fn read_input() -> Vec<Direction> {
     let file_contents = fs::read_to_string("day9_puzzle.txt").expect("Unable to read file");
-    file_contents.split("\n").map(|line| {
+    file_contents.split("\n").flat_map(|line| {
         let (a, b) = line.split_once(" ").unwrap();
-        Move { direction: Direction::from_string(a).unwrap(), distance: b.parse().unwrap() }
+        iter::repeat(Direction::from_string(a).unwrap()).take(b.parse().unwrap())
     }).collect_vec()
 }
 
 #[derive(Debug, Clone)]
 struct RopeState {
-    head: (i32, i32),
-    tail: (i32, i32),
+    knots: Vec<(i32, i32)>,
     visited_tail_positions: HashSet<(i32, i32)>,
 }
 
 impl RopeState {
-    fn new() -> RopeState {
+    fn new(knot_count: usize) -> RopeState {
         RopeState {
-            head: (0, 0),
-            tail: (0, 0),
+            knots: vec![(0, 0); knot_count],
             visited_tail_positions: HashSet::from([(0, 0)]),
         }
     }
 
-    fn apply_move(&mut self, mov: &Move) {
-        assert_eq!(mov.distance, 1);
-
+    fn apply_move(&mut self, direction: &Direction) {
         // Move head.
-        match mov.direction {
-            Direction::Up => self.head.1 += 1,
-            Direction::Down => self.head.1 -= 1,
-            Direction::Left => self.head.0 -= 1,
-            Direction::Right => self.head.0 += 1,
+        {
+            let head = self.knots.first_mut().unwrap();
+            match direction {
+                Direction::Up => head.1 += 1,
+                Direction::Down => head.1 -= 1,
+                Direction::Left => head.0 -= 1,
+                Direction::Right => head.0 += 1,
+            }
         }
 
-        // Move tail.
-        let delta = (self.head.0 - self.tail.0, self.head.1 - self.tail.1);
-        if delta.0 >= 2 {
-            self.tail.0 += 1;
-            self.tail.1 += num::signum(delta.1);
-        } else if delta.0 <= -2 {
-            self.tail.0 -= 1;
-            self.tail.1 += num::signum(delta.1);
-        } else if delta.1 >= 2 {
-            self.tail.1 += 1;
-            self.tail.0 += num::signum(delta.0);
-        } else if delta.1 <= -2 {
-            self.tail.1 -= 1;
-            self.tail.0 += num::signum(delta.0);
+        // Move following knots.
+        {
+            for leading_i in 0..self.knots.len() - 1 {
+                let leading = self.knots[leading_i];
+                let following = self.knots.get_mut(leading_i + 1).unwrap();
+                let delta = (leading.0 - following.0, leading.1 - following.1);
+                if delta.0 >= 2 {
+                    following.0 += 1;
+                    following.1 += num::signum(delta.1);
+                } else if delta.0 <= -2 {
+                    following.0 -= 1;
+                    following.1 += num::signum(delta.1);
+                } else if delta.1 >= 2 {
+                    following.1 += 1;
+                    following.0 += num::signum(delta.0);
+                } else if delta.1 <= -2 {
+                    following.1 -= 1;
+                    following.0 += num::signum(delta.0);
+                }
+            }
         }
 
         // Record tail position.
-        self.visited_tail_positions.insert(self.tail);
+        self.visited_tail_positions.insert(*self.knots.last().unwrap());
     }
 
+    #[allow(dead_code)]
     fn pretty_print(&self) {
-        let min_x = min(self.visited_tail_positions.iter().map(|p| p.0).min().unwrap(), self.head.0);
-        let max_x = max(self.visited_tail_positions.iter().map(|p| p.0).max().unwrap(), self.head.0);
-        let min_y = min(self.visited_tail_positions.iter().map(|p| p.1).min().unwrap(), self.head.1);
-        let max_y = max(self.visited_tail_positions.iter().map(|p| p.1).max().unwrap(), self.head.1);
+        let x_positions = self.visited_tail_positions.iter().map(|p| p.0).chain(self.knots.iter().map(|knot| knot.0));
+        let y_positions = self.visited_tail_positions.iter().map(|p| p.1).chain(self.knots.iter().map(|knot| knot.1));
+        let min_x = x_positions.clone().min().unwrap();
+        let max_x = x_positions.clone().max().unwrap();
+        let min_y = y_positions.clone().min().unwrap();
+        let max_y = y_positions.clone().max().unwrap();
         for y in (min_y..=max_y).rev() {
             for x in min_x..=max_x {
-                let c = if self.head == (x, y) {
-                    "H"
-                } else if self.tail == (x, y) {
-                    "T"
-                } else if (0, 0) == (x, y) {
-                    "s"
-                } else if self.visited_tail_positions.contains(&(x, y)) {
-                    "#"
+                let c = if let Some((knot_i, _)) = self.knots.iter().find_position(|knot| **knot == (x, y)) {
+                    knot_i.to_string()
                 } else {
-                    "."
+                    if (0, 0) == (x, y) {
+                        "s"
+                    } else if self.visited_tail_positions.contains(&(x, y)) {
+                        "#"
+                    } else {
+                        "."
+                    }.to_string()
                 };
                 print!("{}", c);
             }
@@ -112,19 +112,20 @@ impl RopeState {
     }
 }
 
+fn solve_with_knots(knot_count: usize, move_list: &Vec<Direction>) -> usize {
+    let mut state = RopeState::new(knot_count);
+
+    for mov in move_list {
+        state.apply_move(&mov);
+    }
+
+    state.visited_tail_positions.len()
+}
+
 #[allow(dead_code)]
 pub fn day_9() {
     let move_list = read_input();
 
-    let simplified_move_list = move_list.iter().flat_map(|mov| {
-        repeat(Move { direction: mov.direction.clone(), distance: 1 }).take(mov.distance)
-    }).collect_vec();
-
-    let mut state = RopeState::new();
-
-    for mov in simplified_move_list {
-        state.apply_move(&mov);
-    }
-
-    println!("{:?}", state.visited_tail_positions.len());
+    println!("star 1 : {}", solve_with_knots(2, &move_list));
+    println!("star 2 : {}", solve_with_knots(10, &move_list));
 }
